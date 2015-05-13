9682dc4b5abc06c7628362e0b32d66c99bfa6e50
Josh Quintana <josh_d_quintana@homedepot.com>
2015-05-13 15:54:54 -0400

    Karma output html

--- karma.conf.js
+++ karma.conf.js
@@ -67,7 +67,7 @@ module.exports = function ( config ) {
 		// possible values: 'dots', 'progress'
 		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
 		// reporters: [ 'progress' ],
-		reporters: [ 'progress', 'coverage' ],
+		reporters: [ 'progress', 'html', 'coverage' ],
 
 
 		// web server port
